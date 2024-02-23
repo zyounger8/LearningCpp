6e59d39ec7a906a51541734bfe29f08164036ef9
zyounger8 <101150212+zyounger8@users.noreply.github.com>
2024-02-23 19:12:23 +0000

    Need some structure so added some folders

---
+++ decimalPractice/decimalCoversion.cpp
@@ -0,0 +1,24 @@
+// *********************************************************************
+// Write a C++ program that prompts a user to enter three decimal numners
+// The program should add three decimal numbers, convert the sum to the
+// nearest integer, and print the result.
+// *********************************************************************
+
+//header file
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+//declare variables
+  double num1 = 0;
+  double num2 = 0;
+  double num3 = 0;
+  double sum = 0;
+
+  //Statements
+
+  //A user need to enter three decimal numbers
+  
+}
